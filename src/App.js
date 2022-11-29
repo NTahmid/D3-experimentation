18528a06fbeb9abe7148065e5eede020531ae50b
unknown <chowdhurynafis97@gmail.com>
2022-11-29 16:03:53 +0600

    Added Scatter with Lasso. Still WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./Charts/lineChart";
-import LineChart from "./Charts/lineChart";
+// import "./Charts/lineChart";
+import LineChart from "./Charts/LineChart/index";
+import Scatter from "./Charts/Scatter/index";
 function App() {
   return (
     //<div className="App">
+    //<Scatter></Scatter>
     <LineChart></LineChart>
+
     //</div>
   );
 }
